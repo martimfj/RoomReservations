41aa4f2613e732e2903521a5fcafc12567a82e03
Leonardo Pereira Medeiros <leostayner@gmail.com>
2018-09-16 17:47:38 -0600

    implementação main

--- Application/my-app/src/Main.js
+++ Application/my-app/src/Main.js
@@ -1,27 +1,84 @@
 import ReactDom from 'react-dom';
 import React from  'react';
 import MenuAppBar from './components/MenuAppBar'
-import LoginBox from './components/LoginBox';
 import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import AppBar from '@material-ui/core';
 
+import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+
+const styles = {
+    card: {
+      maxWidth: 345
+    },
+    media: {
+      height: 140
+    }
+  };
+  
 export default class Main extends React.Component{
     constructor(){
         super();
         this.state={
-            username:'',
-            password:''
+            salas:[],
+            idSala: 0
         }
     }
+
     
+    getSalas(){      
+        fetch('/main', {
+            method: 'GET'
+        })
+        .then(res => res.json())
+        .then(result => {
+            let perguntas = []
+            for(var i = 0; i < result.length; i++){
+                
+                perguntas.push( 
+                    <Card style = {{maxWidth: 345, margin:10}}  onClick= {this.getIdSala}>
+                            <CardMedia 
+                            />
+                            <CardContent>
+                            <Typography gutterBottom variant="headline" component="h2">
+                                {result[i].nome}
+                            </Typography>
+                            <Typography component="p">
+                                Lugares: {result[i].lugares}
+                            </Typography>
+                            </CardContent>
+                    </Card>
+                    )
+            }
+            this.setState({salas: perguntas})
+            })   
+    }
+
+
+    
+    componentWillMount(){
+        this.getSalas()
+     }
+
+
     render(){
         return(
             <Grid container spacing={16}>
                 <Grid item xs={12}>
-                   <div>
-                       <MenuAppBar/>
-                    </div>
+                    <MenuAppBar/>
+                </Grid>
+
+                <Grid item xs={6}>
+                    {this.state.salas}
+                </Grid>
+
+                <Grid item xs={5}>
+                    <Paper>
+                        dasdsadas
+                    </Paper>
                 </Grid>
             </Grid>
         );
--- Application/route/main.js
+++ Application/route/main.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const router  = express.Router()
 
-router.get('/main', (req, res) =>{
-    res.json({
-        titule: 'primeiro',
-        conteudo: 'cont',
-        autor: 'leo',
-    })
+router.get('/main', function(req, res, next){
+    console.log("main request")
+    req.getConnection(function(err,conn){
+        
+            if (err) return next("Cannot Connect");
+    
+            var query = conn.query('SELECT * FROM salas', function(err,rows){
+    
+                if(err){
+                    console.log(err);
+                    return next("Mysql error, check your query");
+                }
+
+                var result = JSON.parse(JSON.stringify(rows)); 
+                res.json(result);
+    
+                });
+        });
 })
 
 module.exports = router
